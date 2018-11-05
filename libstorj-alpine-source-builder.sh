79033f33e0a61280ef608a46bb74b206bf8d08e4
Akai <akai-z@users.noreply.github.com>
2018-11-05 23:43:14 +0200

    Rename source directory

--- libstorj-alpine-source-builder.sh
+++ libstorj-alpine-source-builder.sh
@@ -3,7 +3,7 @@
 set -Eeuo pipefail
 
 readonly SOURCE_URL="https://api.github.com/repos/%s/releases/%s"
-readonly SOURCE_DIR="libstorj"
+readonly SOURCE_DIR="libstorj-source"
 
 REPOSITORY="storj/libstorj"
 
