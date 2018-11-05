4de72a9e7fbce664471223b01dcf0f2a9a44f383
Akai <akai-z@users.noreply.github.com>
2018-11-05 23:39:49 +0200

    Add build dependencies install function

--- libstorj-alpine-source-builder.sh
+++ libstorj-alpine-source-builder.sh
@@ -6,3 +6,19 @@ readonly SOURCE_URL="https://api.github.com/repos/%s/releases/%s"
 readonly SOURCE_DIR="libstorj"
 
 REPOSITORY="storj/libstorj"
+
+deps_install() {
+  apk update
+  apk add -u --no-cache --virtual .build-deps \
+    autoconf \
+    automake \
+    curl \
+    curl-dev \
+    g++ \
+    json-c-dev \
+    libmicrohttpd-dev \
+    libtool \
+    libuv-dev \
+    make \
+    nettle-dev
+}
